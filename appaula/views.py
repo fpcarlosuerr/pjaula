21544556828a5912eaeab64ceea56514158d6fb6
fpcarlosuerr <fpcarlos@uerr.edu.br>
2024-09-12 20:35:55 -0400

    criando o cadpessoas

--- appaula/views.py
+++ appaula/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+#chama a pagina index
 def index(request):
 
     return render(request,'appaula/index.html')
+
+#chama a pagina cadastro_pessoa.html
+def cadastro_pessoa(request):
+    
+    return render(request,'appaula/cadastro_pessoa.html')
