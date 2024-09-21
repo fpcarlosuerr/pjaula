b40e427e9d75be4ae6a08fe4e3edaafe395448ee
fpcarlosuerr <fpcarlos@uerr.edu.br>
2024-09-21 10:09:28 -0400

    Ajustes da aula 19092024

--- appaula/urls.py
+++ appaula/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('', views.index,name='index'),    
     path('cadastro_pessoa',views.cadastro_pessoa,name="cadpessoa"),
     path('instituicao/novo',views.criar_instituicao,name='criar_instituicao'),
+    path('instituicao/alterar/<int:id>/', views.criar_instituicao, name='editar_instituicao'),
+    path('instituicoes/', views.listar_instituicoes, name='listar_instituicoes'),
 ]
--- appaula/views.py
+++ appaula/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from .models import Instituicao
@@ -5,28 +6,39 @@ from .forms import InstituicaoForm
 
 #Metodo para abrir a página da Instituição para Novo cadastro ou Alteração de um existente
 def criar_instituicao(request, id=None):
-    print("cheguei aqui")
+    # Se 'id' for fornecido, buscar a Instituição específica, senão, criar nova
     instituicao = get_object_or_404(Instituicao, pk=id) if id else None
     lista_instituicoes = Instituicao.objects.all()
+
     if request.method == "POST":
         form = InstituicaoForm(request.POST, instance=instituicao)
         if form.is_valid():
-            form.save()            
-            return redirect('appaula/form_instituicao.html')  
+            form.save()
+            if instituicao:
+                messages.success(request, 'Instituição atualizada com sucesso!')
+            else:
+                messages.success(request, 'Instituição adicionada com sucesso!')
+
+            return redirect('listar_instituicoes')  # Redirecionar para a view de listagem
     else:
         form = InstituicaoForm(instance=instituicao)
+
+    return render(request, 'appaula/form_instituicao.html', {
+        'form': form,
+        'lista_instituicoes': lista_instituicoes,
+        'instituicao': instituicao
+    })
     
-    return render(request, 'appaula/form_instituicao.html', {'form': form,'lista_instituicoes':lista_instituicoes})
-
 #Metodo para listar as instituições cadastradas
-def instituicao_list(request):
-    instituicoes = Instituicao.objects.all()
-    return render(request, 'instituicao_list.html', {'instituicoes': instituicoes})
+def listar_instituicoes(request):
+    instituicao = None
+    lista_instituicoes = Instituicao.objects.all()
+    form = InstituicaoForm(instance=instituicao)
+    return render(request, 'appaula/form_instituicao.html', {
+        'form': form,
+        'lista_instituicoes': lista_instituicoes,
+        'instituicao': instituicao})
 
-#Metodo para lista os setores cadastrados
-def setor_list(request):
-    setores = Setor.objects.all()
-    return render(request, 'setor_list.html', {'setores': setores})
 
 # Create your views here.
 #chama a pagina index
